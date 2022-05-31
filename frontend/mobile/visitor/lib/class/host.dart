7f39cf7905bdbc3fb9ee65946c2bc5996856480a
yu-podong <yu01gun@naver.com>
2022-05-31 23:00:30 +0900

    create host/space class

---
+++ frontend/mobile/visitor/lib/class/host.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Host {
+  String? imagePath;
+  String? name;
+  String? group;
+  String? position;
+
+  Host({
+    required this.imagePath,
+    required this.name,
+    required this.group,
+    required this.position
+  });
+}
---
+++ frontend/mobile/visitor/lib/class/space.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Space {
+  String? roomId;
+  String? roomName;
+  String? isEnable;
+
+  Space({
+    required this.roomId,
+    required this.roomName,
+    required this.isEnable
+  });
+}
