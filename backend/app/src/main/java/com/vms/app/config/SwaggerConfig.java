e40ede476cf27d4affed242e9d9e68afd9f8cbc8
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-07-13 21:39:58 +0900

    Dev swagger 1.0.0

--- backend/app/src/main/java/com/vms/app/config/SwaggerConfig.java
+++ backend/app/src/main/java/com/vms/app/config/SwaggerConfig.java
@@ -31,8 +31,12 @@ public class SwaggerConfig {
 
   private ApiInfo apiInfo() {
     return new ApiInfoBuilder()
-        .title("Practice Swagger")
-        .description("practice swagger config")
+        .title("VMS Open API")
+        .description("VMS API 명세서입니다. 프론트엔드에서 Rest Server로 요청을 보낼 url입니다. 개발에 참고해주세요.\n" +
+            "1. BaseURL : ~~~~~~~\n" +
+            "2. [BaseURL]/[Api] 로 요청을 보내시면 됩니다. e.g. ~~~~~/insertAllData\n" +
+            "3. GET요청 시 Try it Out은 안될 확률이 높습니다.(미숙해요..ㅎㅎ)\n " +
+            "\n\n<b>구현 되지 않은 Api가 있거나, 필요한 기능이 없거나 설명이 부족하거나 헷갈리시면 언제든지 말씀해주세요!</b>")
         .version("1.0.0")
         .build();
   }
--- backend/app/src/main/java/com/vms/app/controller/GuestAppointmentController.java
+++ backend/app/src/main/java/com/vms/app/controller/GuestAppointmentController.java
@@ -14,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vms.app.service.AppointmentService_guest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@Api("방문자의 기능을 담당하는 Controller 입니다")
 @RequestMapping("/guest")
 @RestController
 public class GuestAppointmentController {
@@ -29,8 +32,7 @@ public class GuestAppointmentController {
   @Autowired
   AppointmentService_guest appointmentService_guest;
 
-  // @ApiOperation(value = "hello, world api", notes = "hellow world swagger
-  // check")
+  @ApiOperation(value = "[Button]신청내역", notes = "- 방문자의 신청내역 버튼을 클릭했을때의 기능입니다.\n - <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는 상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n- Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다")
   /*** [GUEST] 신청내역 확인하기 ***/
   @GetMapping("/getMyHistory")
   public Map<String, Object> getMyHistory(Principal principal) {
