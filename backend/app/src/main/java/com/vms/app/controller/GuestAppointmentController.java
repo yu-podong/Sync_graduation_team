cfd023fbb829c07a647bd5e34dcc462c9a875dca
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-07-20 23:08:43 +0900

    Update Swagger UI ver 1.1.0
    현재 구현한 API 명세화

--- backend/app/src/main/java/com/vms/app/controller/GuestAppointmentController.java
+++ backend/app/src/main/java/com/vms/app/controller/GuestAppointmentController.java
@@ -29,10 +29,14 @@ public class GuestAppointmentController {
     return request.getContextPath();
   }
 
+  // public String commonJWTNote = "\n - <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는
+  // 상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n-
+  // Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다";
+
   @Autowired
   AppointmentService_guest appointmentService_guest;
 
-  @ApiOperation(value = "[Button]신청내역", notes = "- 방문자의 신청내역 버튼을 클릭했을때의 기능입니다.\n - <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는 상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n- Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다")
+  @ApiOperation(value = "<Button>[신청내역]", notes = "- 방문자의 [신청내역] 버튼을 클릭했을때의 기능입니다.\n- 승인, 거절, 대기중인 모든 약속이 시간순으로 보여집니다.\n- <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는 상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n- Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다")
   /*** [GUEST] 신청내역 확인하기 ***/
   @GetMapping("/getMyHistory")
   public Map<String, Object> getMyHistory(Principal principal) {
@@ -41,13 +45,17 @@ public class GuestAppointmentController {
   }
 
   /*** [GUEST] 도착 알림 보내기 ***/
+  @ApiOperation(value = "<Button>[도착알림]", notes = "- 방문자가 [도착알림] 버튼을 눌렀을 때의 기능입니다.(figma에서 없어졌네요?!) \n- 방문자가 도착했을 때 해당 버튼을 누르면 접견자에게 도착알림이 전송됩니다.\n- 방문날짜/시각 1시간 전후로는 버튼이 눌리지 않습니다<b>(구현예정)</b>\n- DB의 해당 약속이 isArrival = 1으로 저장됩니다.")
   @PostMapping("/sendArrived")
   public int sendArrived(long appointmentID) {
     return appointmentService_guest.sendArrived(appointmentID);
   }
 
-  /*** [GUEST] 내 약속 가져오기 ***/
-  // (guest 기능에는 없는 것 같은데 혹시 모르니 만들어 놓음)
+  /*** [GUEST] 방문이력 ***/
+  @ApiOperation(value = "<Button>[방문이력]", notes = "- 방문자의 [방문이력] 버튼을 클릭했을 때의 기능입니다.\n- 현재시간을 기준으로 checkout(방문종료시간)이 작은 값, 즉 이미 방문을 마친 약속만 보여집니다.<b>(구현예정)</b>\n - <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는"
+      +
+      "상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n-" +
+      "Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다")
   @GetMapping("/getMyAppointment")
   public Map<String, Object> getMyAppointment(Principal principal) {
 
@@ -55,4 +63,6 @@ public class GuestAppointmentController {
 
     return results;
   }
+
+  // [GUEST] 승인결과 내역 구현할 것 isApproval = 1, 방문이력이랑 다른점 :날짜 체크 하지 않고 전부 가져오기
 }
--- backend/app/src/main/java/com/vms/app/controller/HostAppointmentController.java
+++ backend/app/src/main/java/com/vms/app/controller/HostAppointmentController.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.vms.app.service.AppointmentService_host;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,9 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vms.app.service.AppointmentService_host;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@Api("접견자의 기능을 담당하는 Controller 입니다")
 @RequestMapping("/host")
 @RestController
 public class HostAppointmentController {
@@ -29,7 +32,17 @@ public class HostAppointmentController {
   @Autowired
   private AppointmentService_host appointmentService_host;
 
-  /*** [Host] 요청된 약속 관리 ***/
+  // public String commonJWTNote = "\n - <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는
+  // 상태)만 호출가능합니다.\n - 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n-
+  // Parameter는 필요 없습니다.\n\n- Try out 실행 안될겁니다";
+
+  /*** [Host] 신규접수내역 ***/
+  @ApiOperation(value = "<Button>[신규접수내역]", notes = "- 접견자의 [신규접수내역] 버튼을 눌렀을 때의 기능입니다.\n" +
+      "- 접견자가 아직 처리하지 않은 대기중인 약속(isApproval = 0)인 값만 가져옵니다<b>(구현예정)</b>\n" +
+      "- <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는 상태)만 호출가능합니다.\n" +
+      "- 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n" +
+      "- Parameter는 필요 없습니다.\n\n" +
+      "- Try out 실행 안될겁니다")
   @GetMapping("/getRequestedAppointment")
   public Map<String, Object> getRequestedAppointment(Principal principal) {
 
@@ -39,17 +52,31 @@ public class HostAppointmentController {
 
   /*** [HOST] 약속 승인 ***/
   @PostMapping("/approvalAppointment")
+  @ApiOperation(value = "<Button>[신규접수내역] > ListDetail의 [승인]버튼 클릭시", notes = "- 접견자에게 접수된 약속중 [승인] 버튼을 눌렀을 때의 기능입니다.\n"
+      +
+      "- DB의 isApproval값이 1(승인)로 바뀝니다.\n" +
+      "- 승인 시, 접견자나 방문자의 일정이 중복되면 log를 띄우고 현재 일정을 처리하지 않습니다.<b>(구현예정)</b>")
   public int approvalAppointment(long appointmentID) {
     return appointmentService_host.approvalAppointment(appointmentID);
   }
 
   /*** [HOST] 약속 거절 ***/
   @PostMapping("/rejectAppointment")
+  @ApiOperation(value = "<Button>[신규접수내역] > ListDetail의 [거절]버튼 클릭시", notes = "- 접견자에게 접수된 약속중 [거절] 버튼을 눌렀을 때의 기능입니다.\n"
+      +
+      "- DB의 isApproval값이 1(승인)로 바뀝니다.\n" +
+      "- Parameters 탭의 거절 사유(rejectReason)는 Not Null = false 이기 때문에 굳이 안적어도 되도록 해놨습니다.(무조건 적게 하려면 말씀해주세요!)")
   public int rejectAppointment(long appointmentID, String rejectReason) {
     return appointmentService_host.rejectAppointment(appointmentID, rejectReason);
   }
 
-  /*** [HOST] 내 약속 가져오기 ***/
+  /*** [HOST] 방문기록조회 ***/
+  @ApiOperation(value = "<Button>[방문기록조회]", notes = "- 접견자의 [방문기록조회] 버튼을 클릭했을 때의 기능입니다.\n" +
+      "- 현재시간을 기준으로 checkout(방문종료시간)이 작은 값, 즉 이미 방문을 마친 약속만 보여집니다.<b>(구현예정)</b>\n" +
+      "- <Strong>JWT로 인증된 방문자</Strong>(로그인이 되어 있는 상태)만 호출가능합니다.\n" +
+      "- 프론트엔드에서는 요청시 Header에 발급받은 Token을 실어야지만 정상적으로 요청 가능합니다.\n" +
+      "-Parameter는 필요 없습니다.\n\n" +
+      "- Try out 실행 안될겁니다")
   @GetMapping("/getMyAppointment")
   public Map<String, Object> getMyAppointment(Principal principal) {
     Map<String, Object> results = appointmentService_host.getMyAppointment(principal.getName());
@@ -57,4 +84,5 @@ public class HostAppointmentController {
     return results;
   }
 
+  // 승인 내역 구현할 것 isApproval = 1 방문기록 조회랑 다른점 :날짜 체크 하지 않고 전부 가져오기
 }
