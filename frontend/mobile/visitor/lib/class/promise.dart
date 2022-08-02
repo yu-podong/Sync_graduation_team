d079424428c550b3d39fb97fe8bba93dca9464a4
유현진 <yuhyeonjin@yuhyeonjin-ui-MacBookPro.local>
2022-08-03 00:15:13 +0900

    Create: promise class

---
+++ frontend/mobile/visitor/lib/class/promise.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Promise {
+  String hostName;
+  String position;
+  String place;
+  String time;
+
+  Promise({
+    required this.hostName,
+    required this.position,
+    required this.place,
+    required this.time
+  });
+}
