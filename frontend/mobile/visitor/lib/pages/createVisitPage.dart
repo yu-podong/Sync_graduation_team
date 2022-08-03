28c6d7db73ed4afca0d5fb0e943117bb2055ef55
유현진 <yuhyeonjin@yuhyeonjin-ui-MacBookPro.local>
2022-08-03 23:48:31 +0900

    Modify: component's size & padding,margin

--- frontend/mobile/visitor/lib/pages/createVisitPage.dart
+++ frontend/mobile/visitor/lib/pages/createVisitPage.dart
@@ -22,22 +22,24 @@ class _CreateVisitPage extends State<CreateVisitPage> {
   @override
   void initState() {
     visitPurpose.add(
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(CupertinoIcons.clock),
-            Text('일반내방')
-          ],
-        )
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(CupertinoIcons.alarm),
+          Text('일반내방', style: TextStyle(fontSize: 18),)
+        ],
+      )
     );
     visitPurpose.add(
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(CupertinoIcons.wrench),
-            Text('공사내방')
-          ],
-        )
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(CupertinoIcons.alarm),
+          Text('공사내방', style: TextStyle(fontSize: 18),)
+        ],
+      )
     );
     tapColor.add(CupertinoColors.systemBackground);
     tapColor.add(CupertinoColors.systemBackground);
@@ -63,64 +65,87 @@ class _CreateVisitPage extends State<CreateVisitPage> {
       ),
       /* Body */
       body: CupertinoPageScaffold(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            /* Page title */
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text('방문목적 입력', style: TextStyle(fontSize: 20),),
-            ),
-            /* Description */
-            Padding(
-              padding: EdgeInsets.all(0.0),
-              child: Text('000회사에 방문하려는 목적을 선택해주세요.'),
-            ),
-            /* Visit purpose list */
-            Padding(
-                padding: EdgeInsets.all(0.0),
-                child: Container(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              /* Page title */
+              Container(
+                margin: EdgeInsets.only(bottom: 30),
+                child: Text('방문목적 입력', style: TextStyle(fontSize: 23),),
+              ),
+              /* Description */
+              Container(
+                margin: EdgeInsets.only(bottom: 50),
+                child: Text('000회사에 방문하려는 목적을 선택해주세요.', style: TextStyle(fontSize: 18),),
+              ),
+              /* Visit purpose list */
+              Container(
+                  width: double.infinity,
                   height: 100,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        child: Card(
-                          color: tapColor[index],
-                          child: visitPurpose[index],
-                        ),
-                        onTap: () {
-                          if(index == 0) {
-                            _compose(detailValue1);
-                            tapColor[0] = CupertinoColors.systemYellow;
-                            tapColor[1] = CupertinoColors.systemBackground;
-                          }
-                          else {
-                            _compose(detailValue2);
-                            tapColor[0] = CupertinoColors.systemBackground;
-                            tapColor[1] = CupertinoColors.systemYellow;
-                          }
-                        },
-                      );
-                    },
-                    itemCount: visitPurpose.length,
+                  padding: EdgeInsets.all(0.0),
+                  margin: EdgeInsets.only(bottom: 40),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xffDEDEDE)),
+                      borderRadius: BorderRadius.circular(5)
                   ),
-                )
-            ),
-            /* After choosing */
-            Padding(
-              padding: EdgeInsets.all(0.0),
-              child: detailPurposeSection,
-            ),
-            Padding(
-              padding: EdgeInsets.all(0.0),
-              child: GestureDetector(
+                  child: Column(
+                    children: [
+                      // 일반내방
+                      GestureDetector(
+                        onTap: () {
+                          _compose(detailValue1);
+                          tapColor[0] = CupertinoColors.systemYellow;
+                          tapColor[1] = CupertinoColors.systemBackground;
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: 49,
+                          padding: EdgeInsets.all(0.0),
+                          margin: EdgeInsets.all(0.0),
+                          decoration: BoxDecoration(
+                              color: tapColor[0],
+                              border: Border(
+                                  bottom: BorderSide(width: 0.7, color: Color(0xffDEDEDE))
+                              )
+                          ),
+                          child: visitPurpose[0],
+                        ),
+                      ),
+                      // 공사내방
+                      GestureDetector(
+                        onTap: () {
+                          _compose(detailValue2);
+                          tapColor[0] = CupertinoColors.systemBackground;
+                          tapColor[1] = CupertinoColors.systemYellow;
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: 49,
+                          padding: EdgeInsets.all(0.0),
+                          margin: EdgeInsets.all(0.0),
+                          color: tapColor[1],
+                          child: visitPurpose[1],
+                        ),
+                      )
+                    ],
+                  )
+              ),
+              /* After choosing */
+              Padding(
+                padding: EdgeInsets.all(0.0),
+                child: detailPurposeSection,
+              ),
+              /* Next button */
+              GestureDetector(
                 child: Text('다음 >'),
                 onTap: () {
                   Navigator.of(context).pushReplacementNamed('/createVisit/hostInfo');
                 },
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -129,10 +154,11 @@ class _CreateVisitPage extends State<CreateVisitPage> {
   void _compose(valueList) {
     setState(() {
       detailPurposeSection = Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('방문목적'),
+          Text('방문목적', style: TextStyle(fontSize: 18),),
           Container(
-            height: 400,
+            height: 200,
             child: ListView.builder(
               itemBuilder: (context, index) {
                 return Row(
@@ -146,7 +172,7 @@ class _CreateVisitPage extends State<CreateVisitPage> {
                         });
                       },
                     ),
-                    Text(valueList[index]),
+                    Text(valueList[index], style: TextStyle(fontSize: 18),),
                   ],
                 );
               },
