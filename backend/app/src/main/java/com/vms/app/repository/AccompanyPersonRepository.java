b391d97327f92db859ed9cd569b4f1e3490efdde
ksygt728 <ksygt728@gamil.com>
2022-10-28 14:52:11 +0900

    CREATE: AccompanyPerson.java & AccompanyPersonRepository.java
    동반인원 필드 추가

---
+++ backend/app/src/main/java/com/vms/app/repository/AccompanyPersonRepository.java
@@ -0,0 +1,16 @@
+package com.vms.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vms.app.entity.AccompanyPerson;
+import com.vms.app.entity.User;
+
+@Repository
+public interface AccompanyPersonRepository extends JpaRepository<AccompanyPerson, Long> {
+
+  List<AccompanyPerson> findByGuestOrderByAccompanyPersonIDDesc(User user);
+
+}
