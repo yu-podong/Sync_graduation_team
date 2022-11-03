a5f265c51d3fd6ed0bc97f2ebdecab9b5dcb54fe
yu-podong <yu01gun33@gmail.com>
2022-11-04 02:15:29 +0900

    Add & Modify: new Route link & change old components' name

--- frontend/mobile/visitor/lib/Route.dart
+++ frontend/mobile/visitor/lib/Route.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:mobile/pages/request/companionPage.dart';
+import 'package:mobile/pages/tab/myPage.dart';
+import 'package:mobile/pages/tab/noPage.dart';
 
-import 'pages/visitLoginPage.dart';
-import 'pages/visitorMainPage.dart';
-import 'pages/request/createVisitPage.dart';
-import 'pages/request/general/hostInfoPage.dart';
-import 'pages/request/general/visitDatePage.dart';
-import 'pages/request/general/visitSpacePage.dart';
-import 'pages/request/general/checkVisitInfoPage.dart';
+import 'pages/visLoginPage.dart';
+import 'pages/visMainPage.dart';
+import 'pages/request/reqPurposePage.dart';
+import 'pages/request/general/reqHostPage.dart';
+import 'pages/request/general/reqDatePage.dart';
+import 'pages/request/general/reqSpacePage.dart';
+import 'pages/request/general/reqFinalPage.dart';
+import 'pages/requestList/reqListPage.dart';
+import 'package:mobile/pages/result/resListPage.dart';
+import 'package:mobile/pages/visit/visListPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,15 +24,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      initialRoute: '/login',
+      initialRoute: '/',
       routes: {
-        '/login' : (context) => const VisitLoginPage(),
-        '/' : (context) => const VisitorMainPage(),
-        '/createVisit' : (context) => const CreateVisitPage(),
-        '/createVisit/hostInfo' : (context) => const HostInfoPage(),
-        '/createVisit/visitDate' : (context) => const VisitDatePage(),
-        '/createVisit/visitSpace' : (context) => const VisitSpacePage(),
-        '/createVisit/checkVisitInfo' : (context) => const CheckVisitInfoPage(),
+        '/' : (context) => const VisMainPage(),
+        '/notice' : (context) => const NoPage(),
+        '/my' : (context) => const MyPage(),
+        '/login' : (context) => const VisLoginPage(),
+        '/reqList' : (context) => const ReqListPage(),
+        '/resList' : (context) => const ResListPage(),
+        '/visList' : (context) => const VisListPage(),
+        '/createVisit' : (context) => const ReqPurposePage(),
+        '/createVisit/host' : (context) => const ReqHostPage(),
+        '/createVisit/date' : (context) => const ReqDatePage(),
+        '/createVisit/space' : (context) => const ReqSpacePage(),
+        '/createVisit/companion' : (context) => const CompanionPage(),
+        '/createVisit/final' : (context) => const ReqFinalPage(),
       },
     );
   }
