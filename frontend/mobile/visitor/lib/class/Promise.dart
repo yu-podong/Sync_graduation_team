a02ae1df7a7b435312a7f418f9845bb3313707e2
yu-podong <yu01gun33@gmail.com>
2022-11-05 21:53:33 +0900

    Modify: add isOK attribute of Promise class

--- frontend/mobile/visitor/lib/class/Promise.dart
+++ frontend/mobile/visitor/lib/class/Promise.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/cupertino.dart';
-
 class Promise {
   String hostName;
   String position;
   String place;
+  String date;
   String time;
+  bool isOK;
 
   Promise({
     required this.hostName,
     required this.position,
     required this.place,
-    required this.time
+    required this.date,
+    required this.time,
+    this.isOK = false
   });
 }
